1c2ea105a35c78dd713a91f2c3a9efdb740787ff
Ryan Strat <ryanstrat@users.noreply.github.com>
2014-02-03 13:17:03 -0500

    minor bugfix to index.html and additions to handler.php

--- bigredbutton/handler.php
+++ bigredbutton/handler.php
@@ -5,3 +5,16 @@
     echo $email;
     
 ?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <title>Code Sent!</title>
+        
+    </head>
+    <body>
+        <h1>HTML Content</h1>
+    </body>
+</html>
